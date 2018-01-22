d088966a0f9b7f80fb1b2d38daba5660130da316
Afiq Ramli <afiqramli@Afiqs-MBP.local>
2018-01-22 22:53:56 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+const app = express()
+let currentYear = new Date().getFullYear()
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
+app.use(express.static(__dirname + '/public'))
+
+app.use((req,res,next) => {
+    var timeStamp = new Date().toString()
+    var log = `${timeStamp}: ${req.method} ${req.url}`
+
+    console.log(log)
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log(err)
+        }
+    })
+    next()
+})
+
+hbs.registerHelper('getCurrentYear', () => new Date().getFullYear())
+hbs.registerHelper('screamIt', (text) => text.toUpperCase())
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home page',
+        homeMessage: 'Welcome to this website!'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs',{
+        pageTitle: 'About Page'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('App is running at port 3000')
+})
